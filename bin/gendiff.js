0641822ad385b7a66bc30f6cb96694570df62d94
zalyalova <zalyalova1962@icloud.com>
2026-05-07 17:56:13 +0300

    add json diff formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,7 +12,7 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    genDiff(filepath1, filepath2);
+    console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
+import _ from 'lodash';
 import path from 'path';
 import readFile from './readFile.js';
 import parse from './parser.js';
 
+const formatValue = (value) => {
+  if (typeof value === 'string') {
+    return value;
+  }
+
+  return String(value);
+};
+
 const genDiff = (filepath1, filepath2) => {
-  const data1 = readFile(filepath1);
-  const data2 = readFile(filepath2);
+  const data1 = parse(
+    readFile(filepath1),
+    path.extname(filepath1).slice(1),
+  );
 
-  const format1 = path.extname(filepath1).slice(1);
-  const format2 = path.extname(filepath2).slice(1);
+  const data2 = parse(
+    readFile(filepath2),
+    path.extname(filepath2).slice(1),
+  );
 
-  const parsedData1 = parse(data1, format1);
-  const parsedData2 = parse(data2, format2);
+  const keys = _.sortBy(
+    [...Object.keys(data1), ...Object.keys(data2)],
+  );
 
-  console.log(parsedData1);
-  console.log(parsedData2);
+  const uniqueKeys = _.uniq(keys);
+
+  const lines = uniqueKeys.flatMap((key) => {
+    if (!Object.hasOwn(data2, key)) {
+      return `  - ${key}: ${formatValue(data1[key])}`;
+    }
+
+    if (!Object.hasOwn(data1, key)) {
+      return `  + ${key}: ${formatValue(data2[key])}`;
+    }
+
+    if (data1[key] === data2[key]) {
+      return `    ${key}: ${formatValue(data1[key])}`;
+    }
+
+    return [
+      `  - ${key}: ${formatValue(data1[key])}`,
+      `  + ${key}: ${formatValue(data2[key])}`,
+    ];
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
 };
 
 export default genDiff;
