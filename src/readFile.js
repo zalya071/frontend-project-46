fbdc555def273658e0d3bef87828c9c65dc4e999
zalyalova <zalyalova1962@icloud.com>
2026-05-10 18:12:43 +0300

    fix arrow parens

--- src/readFile.js
+++ src/readFile.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const readFile = (filepath) => {
+const readFile = filepath => {
   const absolutePath = path.resolve(process.cwd(), filepath)
 
   return fs.readFileSync(absolutePath, 'utf-8')
